2fafe9eaebe6fb5045a92ef4688d2ddd5a2b6841
Asfar Khan <asferk8@gmail.com>
2026-06-11 19:35:02 +0530

    commit 1

--- client/src/components/layout/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar(){
-    return(
-        <div className="navbar">
-            <h2>Navbar</h2>
-        </div>
-    );
-}
-
-export default Navbar;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      recharts: fileURLToPath(new URL('./src/lib/recharts-lite.jsx', import.meta.url)),
+    },
+  },
 })
