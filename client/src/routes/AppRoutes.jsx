ecbffdd5b2a9aaf2a6faa24087175455cd5fa502
Asfar Khan <asferk8@gmail.com>
2026-06-25 13:00:11 +0530

    commit 41

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,35 +1,82 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 import Dashboard from "../pages/Dashboard";
 import Expenses from "../pages/Expenses";
 import Budget from "../pages/Budget";
 import Analytics from "../pages/Analytics";
 import AddExpense from "../pages/AddExpense";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ProtectedRoute from "../components/Protectedroute";
 
-function AppRoutes({onAddExpense, month, budget, onSaveBudget, expenses, onDeleteExpense, totalExpenses}) {
+function AppRoutes({onAddExpense, month, budget, onSaveBudget, expenses, onDeleteExpense, totalExpenses, expensesLoading, onUpdateExpense}) {
+    const { isAuthenticated } = useAuth();
+
     return (
         <Routes>
-            <Route path="/" element={
-                <Dashboard
-                    budget={budget}
-                    month={month}
-                    expenses={expenses}
-                    totalExpenses={totalExpenses}
-                />}/>
             <Route
                 path="/login"
+                element={isAuthenticated ? <Navigate to="/" replace /> : <Login />}
+            />
+            <Route
+                path="/register"
+                element={isAuthenticated ? <Navigate to="/" replace /> : <Register />}
+            />
+        
+            <Route
+                path="/"
                 element={
-                    localStorage.getItem("isLoggedIn") === "true" ? (
-                    <Navigate to="/" replace />
-                    ) : (
-                    <Login />
-                    )
+                <ProtectedRoute>
+                    <Dashboard
+                        budget={budget}
+                        month={month}
+                        expenses={expenses}
+                        totalExpenses={totalExpenses}
+                        loading={expensesLoading}
+                    />
+                </ProtectedRoute>
                 }
-                />
-            <Route path="/expenses" element={<Expenses expenses={expenses} onDeleteExpense={onDeleteExpense}/>}/>
-            <Route path="/budget" element={<Budget budget={budget} month={month} onSaveBudget={onSaveBudget}/>}/>
-            <Route path="/analytics" element={<Analytics/>}/>
-            <Route path="/addexpense" element={<AddExpense onAddExpense={onAddExpense}/>}/>
+            />
+            <Route
+                path="/expenses"
+                element={
+                <ProtectedRoute>
+                    <Expenses
+                        expenses={expenses}
+                        onDeleteExpense={onDeleteExpense}
+                        onUpdateExpense={onUpdateExpense}
+                        loading={expensesLoading}
+                    />
+                </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/addexpense"
+                element={
+                <ProtectedRoute>
+                    <AddExpense onAddExpense={onAddExpense} />
+                </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/budget"
+                element={
+                <ProtectedRoute>
+                    <Budget budget={budget} month={month} onSaveBudget={onSaveBudget} />
+                </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/analytics"
+                element={
+                <ProtectedRoute>
+                    <Analytics expenses={expenses} budget={budget} month={month} />
+                </ProtectedRoute>
+                }
+            />
+        
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
 }
--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+]);
 
 const connectDB = async ()=> {
     try {
+        console.log(process.env.MONGO_URI);
         const conn = await mongoose.connect(process.env.MONGO_URI);
 
         console.log(`MongoDB connected: ${conn.connection.host}`);
