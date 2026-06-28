a23db25df12cc7592662a3ea28adea3670c36b92
Asfar Khan <asferk8@gmail.com>
2026-06-28 13:54:35 +0530

    commit 58

--- client/src/pages/Budget.jsx
+++ client/src/pages/Budget.jsx
@@ -59,7 +59,7 @@ function Budget({budget, month, onSaveBudget}){
             <header className="page-header">
                 <p className="over-heading">Plan first</p>
                 <h1>Set Monthly Budget</h1>
-                <p>Choose a realistic monthly spending limit for your student expenses.</p>
+                <p>Choose a realistic monthly spending limit for your expenses.</p>
             </header>
 
             <div className="current-budget-info">
